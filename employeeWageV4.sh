588964424d230cfca06a6408f0cd5fcaae874c5c
Aarti Ahire <61686393+Aarti98765@users.noreply.github.com>
2020-03-15 23:40:32 +0530

    Refactored:Removed the spaces

--- employeeWageV4.sh
+++ employeeWageV4.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -x
+
+#CONSTANT FOR THE PROGRAM
 isPartTime=1;
 isFulltime=2
-empRatePerHr=20
-empCheck=$(( RANDOM%3 ))
+EMP_RATE_PER_HR=20
 
+empCheck=$(( RANDOM%3 ))
 case $empCheck in
-		$isFulltime)
-				empHrs=8;
-				;;
-		$isPartTime)
-				empHrs=4;
-				;;
-		*)
-				empHrs=0;
-				;;
+	$isFulltime)
+		empHrs=8;
+		;;
+	$isPartTime)
+		empHrs=4;
+		;;
+	*)
+		empHrs=0;
+		;;
 esac
 
-salary=$(( $empHrs*$empRatePerHr ))
+salary=$(( $empHrs*$EMP_RATE_PER_HR ))
