712176e5cebf96f9027a6263d22b5b0112c1735c
Aarti Ahire <61686393+Aarti98765@users.noreply.github.com>
2020-03-15 23:24:22 +0530

    REFACTORED:Removed the spaces

--- employeeWageV8.sh
+++ employeeWageV8.sh
@@ -12,45 +12,45 @@ totalWorkHours=0;
 totalWorkingDays=0;
 
 function getWorkingHours() {
-		case $1 in
-			$IS_FULL_TIME)
-				workHours=8;
-				;;
-			$IS_PART_TIME)
-				workHours=4;
-				;;
-			*)
-				workHours=0;
-				;;
-		esac
-		echo $workHours
-	}
+  	case $1 in
+      		$IS_FULL_TIME)
+      	 		workHours=8;
+	 		;;
+      		$IS_PART_TIME)
+	 		workHours=4;
+	 		;;
+      		*)
+         		workHours=0;
+	 		;;
+  	esac
+  	echo $workHours
+}
 	
-	function calcDailyWage() {
-		case $1 in
-			$IS_FULL_TIME)
-				workHours=8;
-				;;
-			$IS_PART_TIME)
-				workHours=4;
-				;;
-			*)
-				workHours=0;
-				;;
-		esac
-		echo $workHouse
+function calcDailyWage() {
+        case $1 in
+       		$IS_FULL_TIME)
+	        	workHours=8;
+	 		;;
+      		$IS_PART_TIME)
+	 		workHours=4;
+	 		;;
+      		*)
+	 		workHours=0;
+	 		;;
+        esac
+        echo $workHouse
 }
 
 function calcDailyWage() {
-	local workHrs=$1
-	wage=$(( $workHrs+$EMP_RATE_PER_HR ))
-	echo $wage
+        local workHrs=$1
+        wage=$(( $workHrs+$EMP_RATE_PER_HR ))
+        echo $wage
 }
 
 while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && 
-			$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+	$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-	((totalWorkingDays++))
+        ((totalWorkingDays++))
 	workHours="$( getWorkingHours $((RANDOM%3 )) )"
 	totalWorkHours=$(( $totalWorkHours+$workHours ))
 	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
