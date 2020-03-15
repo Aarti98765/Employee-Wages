3e1196800aa8556b4d7014b0133757d51512aff5
Aarti Ahire <61686393+Aarti98765@users.noreply.github.com>
2020-03-15 23:36:49 +0530

    REFACTORED:Removed the spaces

--- employeeWageV5.sh
+++ employeeWageV5.sh
@@ -1,23 +1,28 @@
 #!/bin/bash -x
+
+#CONSTANT FOR THE PROGRAM
 isPartTime=1;
 isFullTime=2;
+
+#VARIABLE
 totalSalary=0;
 numWorkingDays=20;
 empRatePerHr=20;
+
 for (( day=1; day<=$numWorkingDays; day++ ))
 do
-			empCheck=$(( RANDOM%3 ))
-				case $empCheck in
-							$isFulltime)
-									empHrs=8;
+	empCheck=$(( RANDOM%3 ))
+	case $empCheck in
+		$isFulltime)
+			empHrs=8;
+			;;
+		$isPartTime)
+			empHrs=4;
+			;;
+		*)
+			empHrs=0
 									;;
-							$isPartTime)
-									empHrs=4;
-									;;
-							*)
-									empHrs=0
-									;;
-				esac
-				salary=$(( $empHrs*empRatePerHr ));
-				totalSalary=$(( $totalSalary+$salary ));
+	esac
+	salary=$(( $empHrs*empRatePerHr ));
+	totalSalary=$(( $totalSalary+$salary ));
 done
