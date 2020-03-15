4f75ded41f75159b046c39f8394463d88c96b08f
Aarti Ahire <61686393+Aarti98765@users.noreply.github.com>
2020-03-15 23:06:49 +0530

    REFACTORED:Removed the spaces

--- empoyeeWageV9.sh
+++ empoyeeWageV9.sh
@@ -13,30 +13,32 @@ totalWorkingDays=0;
 
 function getWorkingHours() {
    case $1 in
-    $IS_FULL_TIME)
-		workHours=8;
-		;;
-	 $IS_PART_TIME)
-		workHours=4;
-		;;
-	 *)
-      workHours=0;
-		;;
-	esac
+   		$IS_FULL_TIME)
+					workHours=8;
+					;;
+	 	$IS_PART_TIME)
+					workHours=4;
+					;;
+	 	*)
+        			workHours=0;
+					;;
+   esac
    echo $workHours
 }
+
 function calcDailyWage() {
-   local workHrs=$1
-	wage=$(( $workHrs*$EMP_RATE_PER_HR ))
-	echo $wage
+		local workHrs=$1
+		wage=$(( $workHrs*$EMP_RATE_PER_HR ))
+		echo $wage
 }
+
 while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH &&
- $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+         $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-   ((totalWorkingDays++))
-   workHours="$( getWorkingHours $((RANDOM%3)) )"
-   totalWorkHours=$(( $totalWorkHours+$workHours ))
-	empDailyWage["$totalWorkingDays"]="$( calcDailyWage $workHours )"
+  		((totalWorkingDays++))
+  		workHours="$( getWorkingHours $((RANDOM%3)) )"
+  		totalWorkHours=$(( $totalWorkHours+$workHours ))
+  		empDailyWage["$totalWorkingDays"]="$( calcDailyWage $workHours )"
 done
 
 totalSalary="$( calcDailyWage $totalWorkHours )"
