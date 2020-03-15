90d29ba2995e65882839179abb450f2e421e36cc
Aarti98765 <aartiahireengit@gmail.com>
2020-03-15 17:30:14 +0530

    ADDED: Calculated wages using while Loop Statement

---
+++ employeeWageV6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x 
+
+#CONSTANT FOR THE PROGRAM
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+#VERIABLES
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHr -le $MAX_HRS_IN_MONTH && $totalWorkingDays -le $NUM_WORKING_DAYS ]]
+do
+			((totalWorkingDays++))
+			empCheck=$(( RANDOM%3 ));
+			case $empCheck in
+							$IS_FULL_TIME)
+									empHr=8;
+									;;
+							$IS_PART_TIME)
+									empHr=4;
+									;;
+							*)
+									empHr=0;
+									;;
+			esac
+			totalEmpHrs=$(( $totalEmpHrs+$empHr ))
+done
+totalSalary=$(( $totalEmpHrs*$EMP_RATE_PER_HR ))
+
