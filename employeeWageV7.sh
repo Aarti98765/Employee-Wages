b6b95f010a8c840e871d07bfc48c767a3ac23573
Aarti Ahire <61686393+Aarti98765@users.noreply.github.com>
2020-03-15 23:28:59 +0530

    REFACTORED:Removed the spaces

--- employeeWageV7.sh
+++ employeeWageV7.sh
@@ -12,24 +12,25 @@ totalEmpHr=0;
 totalWorkingDays=0;
 
 function getWorkingHours() {
-		case $1 in
-			$IS_FULL_TIME)
-						workHours=8;
-						;;
-			$IS_PART_TIME)
-						workHours=4;
-						;;
-			*)
-						workHours=0;
-						;;
-		esac
-		echo $workHours
-	}
-	while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH &&
-				$totalworkingDays -lt $NUM_WORKING_DAYS ]]
-	do
-		((totalWorkingDays++))
-	  workHours="$( getWorkingHours $(( RANDOM%3 )) )"
-	  totalWorkHours=$(( $totalWorkHours+$workHours ))
-	done
-	totalSalary=$(( $totalWorkHours+$EMP_RATE_PER_HR ));
+	case $1 in
+		$IS_FULL_TIME)
+			workHours=8;
+			;;
+		$IS_PART_TIME)
+			workHours=4;
+			;;
+		*)
+			workHours=0;
+			;;
+	esac
+	echo $workHours
+}
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH &&
+	$totalworkingDays -lt $NUM_WORKING_DAYS ]]
+do
+	((totalWorkingDays++))
+	workHours="$( getWorkingHours $(( RANDOM%3 )) )"
+	totalWorkHours=$(( $totalWorkHours+$workHours ))
+done
+
+totalSalary=$(( $totalWorkHours+$EMP_RATE_PER_HR ));
